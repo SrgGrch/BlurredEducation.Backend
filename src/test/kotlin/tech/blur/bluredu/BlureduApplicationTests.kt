2c60a6832d452873bccbe7199e10a9d75065a752
srggrhc <srg.grch@gmail.com>
2019-10-23 23:51:27 +0700

    init

---
+++ src/test/kotlin/tech/blur/bluredu/BlureduApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.blur.bluredu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlureduApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
