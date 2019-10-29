4c024b3b1f81f6aa7dc05ea1cbcc14dbe0cacbdd
srggrhc <srg.grch@gmail.com>
2019-10-29 22:45:40 +0700

    backbone created (for: #8) Connected to heroku postgresql db (for: #2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("org.postgresql:postgresql:42.2.8")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
