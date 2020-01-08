498020d0bd5a7bba1eb0384f1221605f4af5b3f6
srggrhc <srg.grch@gmail.com>
2020-01-09 03:06:14 +0700

    Home page

---
+++ src/main/kotlin/tech/blur/bluredu/api/HomeController.kt
@@ -0,0 +1,23 @@
+package tech.blur.bluredu.api
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import tech.blur.bluredu.core.BaseResponseEntity
+import javax.servlet.http.HttpServletRequest
+
+
+@Controller("HomeController")
+class HomeController {
+
+    @RequestMapping("*")
+    fun hello(request: HttpServletRequest): BaseResponseEntity<String> {
+        println(request.servletPath)
+        return when ((1..100).random()) {
+            in 1..80 -> BaseResponseEntity("Status: OK")
+            in 81..90 -> BaseResponseEntity("Status: NULL")
+            else -> BaseResponseEntity("Status: False")
+
+        }
+    }
+
+}
